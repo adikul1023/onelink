4cbf2995bbae020d0988af493bbac43dfe98130c
Adi K <102898613+adikul1023@users.noreply.github.com>
2025-12-24 14:57:50 +0530

    Configure for GitHub Pages deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,18 @@
 export default ({
     modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@vueuse/nuxt', '@nuxtjs/color-mode'],
-    nitro: {
-        preset: 'cloudflare-pages'
+    ssr: false,
+    app: {
+        baseURL: '/onelink/',
+        head: {
+            charset: 'utf-8',
+            viewport: 'width=device-width, initial-scale=1',
+        }
     },
     build: {
         transpile: ["@headlessui/vue"],
     },
     colorMode: {
         classSuffix: '',
-    },
-    app: {
-        head: {
-            charset: 'utf-8',
-            viewport: 'width=device-width, initial-scale=1',
-        }
     },
     compatibilityDate: '2025-12-24',
 })
