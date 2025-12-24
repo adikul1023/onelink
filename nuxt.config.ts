90741fce92bae5b49144f4c349182510800e4dc1
Adi K <102898613+adikul1023@users.noreply.github.com>
2025-12-24 15:06:03 +0530

    Configure for Vercel deployment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,5 @@
 export default ({
     modules: ['@nuxtjs/tailwindcss', 'nuxt-icon', '@vueuse/nuxt', '@nuxtjs/color-mode'],
-    ssr: false,
-    app: {
-        baseURL: '/onelink/',
-        head: {
-            charset: 'utf-8',
-            viewport: 'width=device-width, initial-scale=1',
-        }
-    },
     build: {
         transpile: ["@headlessui/vue"],
     },
